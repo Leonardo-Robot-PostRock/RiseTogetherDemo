457896b8d3ced5749ccd934c88e4884d64262c9e
Leonardo <leonardo38361@gmail.com>
2026-03-20 11:03:39 -0300

    delete:  legacy implementation

--- src/main/java/com/ITJobsBackend/repository/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ITJobsBackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ITJobsBackend.domain.*;
-
-public interface RolesRepository extends JpaRepository<Roles, Integer>{
-
-}
