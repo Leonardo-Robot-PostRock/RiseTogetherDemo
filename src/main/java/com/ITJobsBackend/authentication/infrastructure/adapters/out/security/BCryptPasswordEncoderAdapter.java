50395e50738c8e63a78429e2a39397a27b716700
Leonardo <leonardo38361@gmail.com>
2026-03-24 13:05:54 -0300

    feat(auth): add JWT token generator, password encoder and security config

---
+++ src/main/java/com/ITJobsBackend/authentication/infrastructure/adapters/out/security/BCryptPasswordEncoderAdapter.java
@@ -0,0 +1,20 @@
+package com.ITJobsBackend.authentication.infrastructure.adapters.out.security;
+
+import com.ITJobsBackend.authentication.application.ports.out.PasswordEncoderPort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptPasswordEncoderAdapter implements PasswordEncoderPort {
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Override
+    public String encode(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
+    }
+}
