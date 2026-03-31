b3b6809186e1933b40338dcf6fd0ebe86bfbff00
Leonardo <leonardo38361@gmail.com>
2026-03-30 22:32:24 -0300

    style(formatting): convert tabs to 4-space indentation in application entry point

--- src/main/java/com/ITJobsBackend/ITJobsBackendApplication.java
+++ src/main/java/com/ITJobsBackend/ITJobsBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ITJobsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ITJobsBackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ITJobsBackendApplication.class, args);
+  }
 }
