5df74557265368cd14bb2c642317958540a0a2f5
Leonardo <leonardo38361@gmail.com>
2026-04-17 23:39:45 -0300

    feat(config): add AuthProperties for JWT configuration

---
+++ src/main/java/com/ITJobsBackend/authentication/infrastructure/config/AuthProperties.java
@@ -0,0 +1,15 @@
+package com.ITJobsBackend.authentication.infrastructure.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
+public class AuthProperties {
+
+  private int unverifiedUserExpirationHours = 12;
+}
